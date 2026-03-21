b8008905e2431394e1472490fff60c8200c567ad
Roman-Ya-n <roman.yatsyshyn.shi.2024@lpnu.ua>
2026-03-21 22:39:17 +0200

    Second Task. Serializers. Commit #2. TO_REVIEW

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -4,6 +4,6 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
-        
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined', 'last_login']
+        read_only_fields = ['id', 'is_staff', 'is_active', 'date_joined', 'last_login']        
     
